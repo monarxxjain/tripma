938d3756fcb2530bb1dd055a6b44a393e5fa7ad3
monarxxjain <jainmonark1000@gmail.com>
2023-12-22 19:35:20 +0530

    Build error Fixing

--- src/components/molecules/departure/Filters.jsx
+++ src/components/molecules/departure/Filters.jsx
@@ -2,57 +2,54 @@
 import React, { useRef } from 'react'
 import Image from 'next/image'
 import Drop from '@/assets/icons/drop-down.svg'
-
 const Filters = () => {
     const filters = [
         {
             name: "Max Price",
-            options: [100, 200, 300]
+            options: [100,200,300]
         },
         {
             name: "Shops",
-            options: [100, 200, 300]
+            options: [100,200,300]
         },
         {
             name: "Times",
-            options: [100, 200, 300]
+            options: [100,200,300]
         },
         {
             name: "Airlines",
-            options: [100, 200, 300]
+            options: [100,200,300]
         },
         {
             name: "Seat class",
-            options: [100, 200, 300]
+            options: [100,200,300]
         },
         {
             name: "More",
-            options: [100, 200, 300]
+            options: [100,200,300]
         },
     ]
-
-    // Use an array to store refs for each dropdown
-    const dropdownRefs = filters.map(() => useRef());
-
-    return (
-        <div className='custom-dropdown flex overflow-x-scroll gap-4 mt-6 hide-scroller'>
-            {filters.map((filter, index) => {
-                return (
-                    <div key={index} className='py-2 px-3 shrink-0 flex justify-between rounded border border-[#CBD4E6]'>
-                        <select ref={dropdownRefs[index]} className='z-10 bg-transparent'>
-                            <option value="">{filter.name}</option>
-                            {filter.options.map((option, id) => {
-                                return (
-                                    <option key={id} value={option}>{option}</option>
+  return (
+    <div  className='custom-dropdown flex overflow-x-scroll gap-4 mt-6 hide-scroller'>
+        {filters.map((filter, index)=>{
+            // eslint-disable-next-line react-hooks/rules-of-hooks
+            const dropDown = useRef()
+            return (
+                <div key={index} className='py-2 px-3 shrink-0 flex justify-between rounded border border-[#CBD4E6]'>
+                    <select  ref={dropDown} className='z-10 bg-transparent'>
+                        <option value="">{filter.name}</option>
+                        {filter.options.map((option, id)=>{
+                            return (
+                                <option key={id} value={option}>{option}</option>
                                 )
                             })}
-                        </select>
-                        <Image src={Drop} onClick={() => dropdownRefs[index].current.click()} className='cursor-pointer ms-[-15px]' />
-                    </div>
-                )
-            })}
-        </div>
-    )
+                    </select>
+                    <Image src={Drop} onClick={()=>dropDown.current.click()} className='cursor-pointer ms-[-15px]' />
+                </div>
+            )
+        })}
+    </div>
+  )
 }
 
 export default Filters
